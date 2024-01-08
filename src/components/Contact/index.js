cf2012cd1a0be1ad0f3018e2ba8dde66a3f70077
koundinya <koundinya511@gmail.com>
2024-01-07 20:11:07 -0500

    Added Store and Theme for Web project

--- src/components/Contact/index.js
+++ src/components/Contact/index.js
@@ -1,7 +1,26 @@
 import React from "react";
-
+import Button from "@mui/material/Button";
+import Grid from "@mui/material/Grid";
+import { useSelector, useDispatch } from "react-redux";
+import { TEST_ACTION } from "actions";
 const Contact = () => {
-  return <div>Contact</div>;
+  const test = useSelector((state) => state.test);
+  const dispatch = useDispatch();
+  const handleButtonClick = () => {
+    dispatch({
+      type: TEST_ACTION.PENDING,
+      payload: "My Text",
+    });
+  };
+  console.log(test);
+  return (
+    <Grid container>
+      <Grid item>
+        <Button onClick={handleButtonClick}>Click Me</Button>
+      </Grid>
+      <Grid item>{test}</Grid>
+    </Grid>
+  );
 };
 
 export default Contact;
